a4e18a1217625711011058e798cf362e87779efc
koji <ishii4itsp@gmail.com>
2017-06-02 17:11:18 +0900

    Receive an implicit intent

--- app/src/main/java/jp/ac/titech/itpro/sdl/activityuser/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/activityuser/MainActivity.java
@@ -35,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
                 request = getString(R.string.request_3_text);
                 break;
         }
-        Intent intent = new Intent();
-        intent.setClassName("jp.ac.titech.itpro.sdl.activityprovider",
-                "jp.ac.titech.itpro.sdl.activityprovider.PublicActivity");
+        Intent intent = new Intent("jp.ac.titech.itpro.sdl.activityprovider.MY_ACTION");
+        // intent.setClassName("jp.ac.titech.itpro.sdl.activityprovider",
+        //         "jp.ac.titech.itpro.sdl.activityprovider.PublicActivity");
         intent.putExtra("request", request);
         startActivityForResult(intent, MYREQCODE);
     }
